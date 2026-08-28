cbe306a184fbd3156b0c2895e5b1e474c4964aee
Rodion Rybko <rybkorodion201@mail.ru>
2026-08-28 17:30:43 +0700

    доработал обертку запроса

--- src/api/fetch/request.ts
+++ src/api/fetch/request.ts
@@ -3,7 +3,7 @@ import {buildUrl} from "./functions/buildUrl.ts";
 
 export const createRequest = (baseUrl: string) => {
 
-    const request = async <ReturnedType, T = never>({ url, queryParams, body, getAuth, method }: MethodParams<T>): Promise<ReturnedType> => {
+    const request = async <ReturnedType, BodyType = never>({ url, queryParams, body, getAuth, method }: MethodParams<BodyType>): Promise<ReturnedType> => {
         const prepairedUrl = buildUrl(baseUrl, url, queryParams);
 
         const headers = {
@@ -15,18 +15,27 @@ export const createRequest = (baseUrl: string) => {
         const response = await fetch(prepairedUrl, { method: method, headers: headers, body: JSON.stringify(body) });
 
         if (!response.ok) {
-            // Временно
-            console.error(response.statusText)
+            console.error(response.statusText);
         }
 
-        return response.json()
+        return await response.json();
     }
 
     return {
-        get: async <ReturnedType, T = never>(params: RequestParams<T>): Promise<ReturnedType> => {
-            const result = await request<ReturnedType, T>({ ...params, method: 'GET'})
+        get: async <ReturnedType, BodyType = never>(params: RequestParams<BodyType>): Promise<ReturnedType> => {
+            const result = await request<ReturnedType, BodyType>({ ...params, method: 'GET'})
 
-            return result
-        }
+            return result;
+        },
+        post: async <ReturnedType, BodyType>(params: RequestParams<BodyType>): Promise<ReturnedType> => {
+            const result = await request<ReturnedType, BodyType>({ ...params, method: 'POST' })
+
+            return result;
+        },
+        put: async <ReturnedType, BodyType>(params: RequestParams<BodyType>): Promise<ReturnedType> => {
+            const result = await request<ReturnedType, BodyType>({ ...params, method: 'PUT' })
+
+            return result;
+        },
     }
 }
